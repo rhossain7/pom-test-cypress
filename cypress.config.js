00575c61c00d363f1109af7d5845adb38df24af5
rhossain7 <reedwan.hossan@rootnext.com>
2023-09-25 13:16:45 +0600

    added gh workflow config and updated cypress config

--- cypress.config.js
+++ cypress.config.js
@@ -1,6 +1,8 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+
+  projectId: "tjh3as",
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
